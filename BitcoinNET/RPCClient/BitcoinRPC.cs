3765cfa1fb2df383ac3e4f92d7ed4cacf2cae949
mb300sd <mb300sd@github>
2012-06-27 19:29:29 -0400

    merging some changes

--- BitcoinNET/RPCClient/BitcoinRPC.cs
+++ BitcoinNET/RPCClient/BitcoinRPC.cs
@@ -8,11 +8,11 @@ using Newtonsoft.Json;
 
 namespace BitcoinNET.RPCClient
 {
-	public class BitcoinRPC
+	public partial class BitcoinRPC
 	{
-		private Uri uri;
+		protected Uri uri;
 
-		private NetworkCredential credentials;
+		protected NetworkCredential credentials;
 
 		public BitcoinRPC(Uri _Uri, NetworkCredential _Credentials)
 		{
@@ -20,7 +20,7 @@ namespace BitcoinNET.RPCClient
 			credentials = _Credentials;
 		}
 
-		public T RpcCall<T>(RPCRequest rpcRequest)
+		protected string HttpCall(string jsonRequest)
 		{
 			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
 
@@ -34,9 +34,8 @@ namespace BitcoinNET.RPCClient
 
 			//webRequest.Credentials = Credentials;
 
-			string jsonRequest = JsonConvert.SerializeObject(rpcRequest);
-
 			request.ContentLength = jsonRequest.Length;
+
 			using (StreamWriter sw = new StreamWriter(request.GetRequestStream()))
 			{
 				sw.Write(jsonRequest);
@@ -47,8 +46,7 @@ namespace BitcoinNET.RPCClient
 				using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
 				using (StreamReader sr = new StreamReader(response.GetResponseStream()))
 				{
-					RPCResponse<T> rpcResponse = JsonConvert.DeserializeObject<RPCResponse<T>>(sr.ReadToEnd());
-					return rpcResponse.result;
+					return sr.ReadToEnd();
 				}
 			}
 			catch (WebException wex)
@@ -60,316 +58,24 @@ namespace BitcoinNET.RPCClient
 					{
 						throw;
 					}
-					RPCResponse<Object> rpcResponse = JsonConvert.DeserializeObject<RPCResponse<Object>>(sr.ReadToEnd());
-					throw new BitcoinRPCException(rpcResponse.error, wex);
+					return sr.ReadToEnd();
 				}
 			}
 		}
 
-		public string AddMultiSigAddress(int NRequired, IEnumerable<string> keys, string Account = "")
+		private T RpcCall<T>(RPCRequest rpcRequest)
 		{
-			return RpcCall<string>
-				(new RPCRequest("addmultisigaddress", new Object[] { NRequired, keys, Account }));
-		}
+			string jsonRequest = JsonConvert.SerializeObject(rpcRequest);
 
-		public void BackupWallet(string Destination)
-		{
-			RpcCall<object>
-				(new RPCRequest("backupwallet", new Object[] { Destination }));
-		}
+			string result = HttpCall(jsonRequest);
 
-		public string DumpPrivKey(string BitcoinAddress)
-		{
-			return RpcCall<string>
-				(new RPCRequest("dumpprivkey", new Object[] { BitcoinAddress }));
-		}
+			RPCResponse<T> rpcResponse = JsonConvert.DeserializeObject<RPCResponse<T>>(result);
 
-		public void EncryptWallet(string Passphrase)
-		{
-			RpcCall<Object>
-				(new RPCRequest("encryptwallet", new Object[] { Passphrase }));
-		}
-
-		public string GetAccount(string BitcoinAddress)
-		{
-			return RpcCall<string>
-				(new RPCRequest("getaccount", new Object[] { BitcoinAddress }));
-		}
-
-		public string GetAccountAddress(string Account)
-		{
-			return RpcCall<string>
-				(new RPCRequest("getaccountaddress", new Object[] { Account }));
-		}
-
-		public IEnumerable<string> GetAddressesByAccount(string Account)
-		{
-			return RpcCall<IEnumerable<string>>
-				(new RPCRequest("getaddressesbyaccount", new Object[] { Account }));
-		}
-
-		public decimal GetBalance(string Account = null, int MinConf = 1)
-		{
-			if (Account == null)
+			if (rpcResponse.error != null)
 			{
-				return RpcCall<decimal>
-				(new RPCRequest("getbalance"));
+				throw new BitcoinRPCException(rpcResponse.error);
 			}
-			return RpcCall<decimal>
-				(new RPCRequest("getbalance", new Object[] { Account, MinConf }));
-		}
-
-		public GetBlockResponse GetBlock(string Hash)
-		{
-			return RpcCall<GetBlockResponse>
-				(new RPCRequest("getblock", new Object[] { Hash }));
-		}
-
-		public int GetBlockCount()
-		{
-			return RpcCall<int>
-				(new RPCRequest("getblockcount"));
-		}
-
-		public string GetBlockHash(long Index)
-		{
-			return RpcCall<string>
-				(new RPCRequest("getblockhash", new Object[] { Index }));
-		}
-
-		public int GetConnectionCount()
-		{
-			return RpcCall<int>
-				(new RPCRequest("getconnectioncount"));
-		}
-
-		public decimal GetDifficulty()
-		{
-			return RpcCall<decimal>
-				(new RPCRequest("getdifficulty"));
-		}
-
-		public bool GetGenerate()
-		{
-			return RpcCall<bool>
-				(new RPCRequest("getgenerate"));
-		}
-
-		public decimal GetHashesPerSec()
-		{
-			return RpcCall<decimal>
-				(new RPCRequest("gethashespersec"));
-		}
-
-		public GetInfoResponse GetInfo()
-		{
-			return RpcCall<GetInfoResponse>
-				(new RPCRequest("getinfo"));
-		}
-
-		public GetMemoryPoolResponse GetMemoryPool()
-		{
-			return RpcCall<GetMemoryPoolResponse>
-				(new RPCRequest("getmemorypool"));
-		}
-
-		public bool GetMemoryPool(string Data)
-		{
-			return RpcCall<bool>
-				(new RPCRequest("getmemorypool", new Object[] { Data }));
-		}
-
-		public GetMiningInfoResponse GetMiningInfo()
-		{
-			return RpcCall<GetMiningInfoResponse>
-				(new RPCRequest("getinfo"));
-		}
-
-		public string GetNewAddress(string Account = "")
-		{
-			return RpcCall<string>
-				(new RPCRequest("getnewaddress", new Object[] { Account }));
-		}
-
-		public decimal GetReceivedByAccount(string Account, int MinConf = 1)
-		{
-			return RpcCall<decimal>
-				(new RPCRequest("getreceivedbyaccount", new Object[] { Account, MinConf }));
-		}
-
-		public decimal GetReceivedByAddress(string BitcoinAddress, int MinConf = 1)
-		{
-			return RpcCall<decimal>
-				(new RPCRequest("getreceivedbyaddress", new Object[] { BitcoinAddress, MinConf }));
-		}
-
-		public GetTransactionResponse GetTransaction(string TxID)
-		{
-			return RpcCall<GetTransactionResponse>
-				(new RPCRequest("gettransaction", new Object[] { TxID }));
-		}
-
-		public GetWorkResponse GetWork()
-		{
-			return RpcCall<GetWorkResponse>
-				(new RPCRequest("getwork"));
-		}
-
-		public bool GetWork(string Data)
-		{
-			return RpcCall<bool>
-				(new RPCRequest("getwork", new Object[] { Data }));
-		}
-
-		public string Help(string Command = "")
-		{
-			return RpcCall<string>
-				(new RPCRequest("help", new Object[] { Command }));
-		}
-
-		public void ImportPrivKey(string BitcoinPrivKey, string Label = "")
-		{
-			RpcCall<Object>
-				(new RPCRequest("importprivkey", new Object[] { BitcoinPrivKey, Label }));
-		}
-
-		public void KeyPoolRefill()
-		{
-			RpcCall<object>
-				(new RPCRequest("keypoolrefill"));
-		}
-
-		public IDictionary<string, decimal> ListAccounts(int MinConf = 1)
-		{
-			return RpcCall<IDictionary<string, decimal>>
-				(new RPCRequest("listaccounts", new Object[] { MinConf }));
-		}
-
-		public IEnumerable<IEnumerable<ListAddressGroupingsResponse>> ListAddressGroupings(bool showEmptyGroups = true, bool showEmptyAddresses = true)
-		{
-			return RpcCall<IEnumerable<IEnumerable<ListAddressGroupingsResponse>>>
-				(new RPCRequest("listaddressgroupings", new Object[] { showEmptyGroups, showEmptyAddresses} ));
-		}
-
-		public IEnumerable<ListReceivedByAccountResponse> ListReceivedByAccount(int MinConf = 1, bool IncludeEmpty = false)
-		{
-			return RpcCall<IEnumerable<ListReceivedByAccountResponse>>
-				(new RPCRequest("listreceivedbyaccount", new Object[] { MinConf, IncludeEmpty }));
-		}
-
-
-		public IEnumerable<ListReceivedByAddressResponse> ListReceivedByAddress(int MinConf = 1, bool IncludeEmpty = false)
-		{
-			return RpcCall<IEnumerable<ListReceivedByAddressResponse>>
-				(new RPCRequest("listreceivedbyaddress", new Object[] { MinConf, IncludeEmpty }));
-		}
-
-		public ListSinceBlockResponse ListSinceBlock(string BlockHash = null, int TargetConfirmations = 1)
-		{
-			if (BlockHash == null)
-			{
-				return RpcCall<ListSinceBlockResponse>
-					(new RPCRequest("listsinceblock"));
-			}
-			return RpcCall<ListSinceBlockResponse>
-				(new RPCRequest("listsinceblock", new Object[] { BlockHash, TargetConfirmations }));
-		}
-
-		public IEnumerable<ListTransactionsResponse> ListTransactions(string Account = "*", int Count = 10, int From = 0)
-		{
-			return RpcCall<IEnumerable<ListTransactionsResponse>>
-				(new RPCRequest("listtransactions", new Object[] { Account, Count, From }));	
-		}
-
-		public bool Move(string FromAccount, string ToAccount, decimal Amount, int MinConf = 1, string Comment = "")
-		{
-			return RpcCall<bool>
-				(new RPCRequest("move", new Object[] { FromAccount, ToAccount, Amount, MinConf, Comment }));
-		}
-
-		public string SendFrom(string FromAccount, string ToBitcoinAddress, decimal Amount, int MinConf = 1, string Comment = "", string CommentTo = "")
-		{
-			return RpcCall<string>
-				(new RPCRequest("sendfrom", new Object[] { FromAccount, ToBitcoinAddress, Amount, MinConf, Comment, CommentTo }));
-		}
-
-		public string SendMany(string FromAccount, IDictionary<string, decimal> ToBitcoinAddresses, int MinConf = 1, string Comment = "")
-		{
-			return RpcCall<string>
-				(new RPCRequest("sendmany", new Object[] { FromAccount, ToBitcoinAddresses, MinConf, Comment }));
-		}
-
-		public string SendMany(string FromAccount, IEnumerable<string> FromAddresses, IDictionary<string, decimal> ToBitcoinAddresses, int MinConf = 1, string Comment = "")
-		{
-			IEnumerable<string> AccountAddresses = (new string[] { FromAccount }).Concat(FromAddresses);
-			return RpcCall<string>
-				(new RPCRequest("sendmany", new Object[] { AccountAddresses, ToBitcoinAddresses, MinConf, Comment }));
-		}
-
-		public string SendToAddress(string BitcoinAddress, decimal Amount, string Comment = "", string CommentTo = "")
-		{
-			return RpcCall<string>
-				(new RPCRequest("sendtoaddress", new Object[] { BitcoinAddress, Amount, Comment, CommentTo }));
-		}
-
-		public void SetAccount(string BitcoinAddress, string Account)
-		{
-			RpcCall<Object>
-				(new RPCRequest("setaccount", new Object[] { BitcoinAddress, Account }));
-		}
-
-		public void SetGenerate(bool Generate, int GenProcLimit = 1)
-		{
-			RpcCall<Object>
-				(new RPCRequest("setgenerate", new Object[] { Generate, GenProcLimit }));
-		}
-
-		public bool SetTxFee(decimal Amount)
-		{
-			return RpcCall<bool>
-				(new RPCRequest("settxfee", new Object[] { Amount }));
-		}
-
-		public string SignMessage(string BitcoinAddress, string Message)
-		{
-			return RpcCall<string>
-				(new RPCRequest("signmessage", new Object[] { BitcoinAddress, Message }));
-		}
-
-		public void Stop()
-		{
-			RpcCall<Object>
-				(new RPCRequest("stop"));
-		}
-
-		public ValidateAddressResponse ValidateAddress(string Address)
-		{
-			return RpcCall<ValidateAddressResponse>
-				(new RPCRequest("validateaddress", new Object[] { Address }));
-		}
-
-		public void WalletLock()
-		{
-			RpcCall<Object>
-				(new RPCRequest("walletlock"));
-		}
-
-		public void WalletPassphrase(string Passphrase, int Timeout)
-		{
-			RpcCall<Object>
-				(new RPCRequest("walletpassphrase", new Object[] { Passphrase, Timeout }));
-		}
-
-		public void WalletPassphraseChange(string OldPassphrase, string NewPassphrase)
-		{
-			RpcCall<Object>
-				(new RPCRequest("walletpassphrasechange", new Object[] { OldPassphrase, NewPassphrase }));
-		}
-
-		public bool VerifyMessage(string BitcoinAddress, string Signature, string Message)
-		{
-			return RpcCall<bool>
-				(new RPCRequest("verifymessage", new Object[] { BitcoinAddress, Signature, Message }));
+			return rpcResponse.result;
 		}
 	}
 }
